cfbdafba0a867059ed90bd0fe7fa0dd8d8f97f0a
gantian127 <jamy127@foxmail.com>
2023-04-04 17:18:37 -0600

    disable version

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
-import versioneer
+#import versioneer
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="bmi_roms",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    version='beta0.1',
+    #version=versioneer.get_version(),
+    #cmdclass=versioneer.get_cmdclass(),
     author="Tian Gan",
     author_email="jamy127@foxmail.com",
     description="BMI implementation for ROMS model data https://www.myroms.org/",
