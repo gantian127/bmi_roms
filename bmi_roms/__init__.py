8e9f03389221c8764874b0004a6f025341b6ec76
gantian127 <jamy127@foxmail.com>
2023-04-11 16:44:04 -0600

    add version info

--- bmi_roms/__init__.py
+++ bmi_roms/__init__.py
@@ -1,13 +1,12 @@
+from ._version import __version__
 from .utils import RomsData
 from .bmi import BmiRoms
 from .errors import RomsError, DataError
-# from ._version import get_versions
 
-__all__ = ["RomsData",
+
+__all__ = ["__version__",
+           "RomsData",
            "BmiRoms",
            "DataError",
            "RomsError"
            ]
-
-# __version__ = get_versions()['version']
-# del get_versions
